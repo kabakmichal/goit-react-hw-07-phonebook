98c9e7c62c5ed85686252415ba64e7fa5f110ce2
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-12-21 10:55:21 +0100

    almost end

---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,30 @@
+import styles from './Filter.module.css';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { handleFilter } from 'redux/contacts/contactsSlice';
+
+export const Filter = () => {
+  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(handleFilter(filter));
+  }, [filter]);
+
+  const handleChange = e => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <label className={styles.label}>
+      Find contacts by Name
+      <input
+        className={styles.input}
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={handleChange}
+      />
+    </label>
+  );
+};
